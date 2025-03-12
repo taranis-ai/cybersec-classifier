89a7bc9e53e515292db1a6fd8ccb1670ea5161c2
llinauer <l.linauer@gmx.at>
2025-03-12 15:41:11 +0100

    Implement bart_mnli cybersecurity classifier

--- cybersec_classifier/bart_mnli.py
+++ cybersec_classifier/bart_mnli.py
@@ -1,17 +1,24 @@
-from cybersec_classifier.config import Config
 from cybersec_classifier.predictor import Predictor
-# import model libraries
+from transformers import pipeline
+from cybersec_classifier.log import logger
 
-class BartMnli(Predictor):
 
-    # add huggingface model name (e.g. facebook/bart-large-cnn)
-    # needed for using the modelinfo endpoint
-    model_name = None
+class BartMNLIClassifier(Predictor):
+    model_name = "facebook/bart-large-mnli"
+    candidate_labels = ["security", "non-security"]
 
     def __init__(self):
-        # instantiate model here
-        self.model = None
+        self.model = pipeline("zero-shot-classification", model=self.model_name)
 
-    def predict(self):
-        # add inference code here
-        raise NotImplementedError("The class BartMnli must implement the 'predict' method")
+    def predict(self, text: str) -> dict:
+        if not text:
+            logger.error("No text input provided for classificaiton")
+            return dict(zip(self.candidate_labels, [0.0, 0.0]))
+
+        classification_result = self.model(text, self.candidate_labels)
+        scores = classification_result.get("scores", None)
+        if not scores or not isinstance(scores, list):
+            logger.error("Classifier did not produce valid classification scores")
+            scores = [0.0, 0.0]
+
+        return dict(zip(self.candidate_labels, scores))
