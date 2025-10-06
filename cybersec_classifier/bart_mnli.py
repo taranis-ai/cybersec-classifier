bac8b73931d8fdb34837b3dcab178fd4097ff270
llinauer <l.linauer@gmx.at>
2025-10-06 13:34:20 +0200

    Adapt MLPClassifier and BartMNLIClassifier to adhere to the protocol defined in taranis-base-bot

--- cybersec_classifier/bart_mnli.py
+++ cybersec_classifier/bart_mnli.py
@@ -1,15 +1,19 @@
-from cybersec_classifier.predictor import Predictor
 from transformers import pipeline
 
 
-class BartMNLIClassifier(Predictor):
-    model_name = "facebook/bart-large-mnli"
-    candidate_labels = ["cybersecurity", "non-cybersecurity"]
+class BartMNLIClassifier:
+    """
+    Zero-shot classifier using BART-MNLI
+    Model URL: https://huggingface.co/facebook/bart-large-mnli
+    """
 
-    def __init__(self):
+    model_name = "facebook/bart-large-mnli"
+    candidate_labels = ["non-cybersecurity", "cybersecurity"]
+
+    def __init__(self) -> None:
         self.model = pipeline("zero-shot-classification", model=self.model_name)
 
-    def predict(self, text: str) -> dict:
+    def predict(self, text: str, **_: object) -> dict:
         classification_result = self.model(text, self.candidate_labels, multi_label=True)
         predicted_labels = classification_result.get("labels", [])
         scores = classification_result.get("scores", [])
--- cybersec_classifier/cybersec_mlp.py
+++ cybersec_classifier/cybersec_mlp.py
@@ -1,19 +1,24 @@
-from cybersec_classifier.predictor import Predictor
 from sentence_transformers import SentenceTransformer
 from huggingface_hub import hf_hub_download
 import joblib
 
 
-class MLPClassifier(Predictor):
+class MLPClassifier:
+    """
+    Classifier based on embed + MLP
+    URL: https://huggingface.co/selfconstruct3d/cybersec_classifier
+
+    """
+
     model_name = "selfconstruct3d/cybersec_classifier"
     candidate_labels = ["non-cybersecurity", "cybersecurity"]
 
-    def __init__(self):
+    def __init__(self) -> None:
         model_path = hf_hub_download(repo_id=self.model_name, filename="cybersec_classifier.pkl")
         self.model = joblib.load(model_path)
         self.embedder = SentenceTransformer("intfloat/multilingual-e5-large")
 
-    def predict(self, text: str) -> dict:
+    def predict(self, text: str, **_: object) -> dict[str, float]:
         embedding = self.embedder.encode([text], convert_to_numpy=True, show_progress_bar=False)
         if scores := self.model.predict_proba(embedding).tolist()[0]:
             return dict(zip(self.candidate_labels, scores))
