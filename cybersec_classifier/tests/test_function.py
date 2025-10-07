7c4505c45250e94cdecfd6613246a4c0aa76bb1f
llinauer <l.linauer@gmx.at>
2025-10-07 12:04:59 +0200

    Reduce tests to specifics for this repo only

--- cybersec_classifier/tests/test_function.py
+++ cybersec_classifier/tests/test_function.py
@@ -2,6 +2,13 @@ from cybersec_classifier.bart_mnli import BartMNLIClassifier
 from cybersec_classifier.cybersec_mlp import MLPClassifier
 
 
+def test_request_parser_error(client):
+    response = client.post("/", json={"another_key": ""})
+    assert response.status_code == 400
+    data = response.get_json()
+    assert data["error"] == "No text provided for cybersecurity classification"
+
+
 def test_cybersec_classification_bart_mnli(bart_mnli: BartMNLIClassifier):
     result = bart_mnli.predict("This is a general text about flowers.")
     assert isinstance(result, dict)
