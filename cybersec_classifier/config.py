71e2181d12678c41179ecca9d8c7f0b83b82e392
llinauer <l.linauer@gmx.at>
2025-10-15 10:41:11 +0200

    Remove get_model func from config; add needed confs for taranis-base-bot

--- cybersec_classifier/config.py
+++ cybersec_classifier/config.py
@@ -1,25 +1,12 @@
-from functools import lru_cache
 from typing import Literal
-from taranis_base_bot.protocols import Predictor
 from taranis_base_bot.config import CommonSettings
 
 
 class Settings(CommonSettings):
-    MODEL: Literal["mlp", "bart_mnli"] = "mlp"
+    MODEL: Literal["cybersec_mlp", "bart_mnli"] = "cybersec_mlp"
+    PACKAGE_NAME: str = "cybersec_classifier"
+    HF_MODEL_INFO: bool = True
+    PAYLOAD_KEY: str = "text"
 
 
 Config = Settings()
-
-
-@lru_cache(maxsize=1)
-def get_model() -> Predictor:
-    if Config.MODEL == "mlp":
-        from cybersec_classifier.cybersec_mlp import MLPClassifier
-
-        return MLPClassifier()
-    elif Config.MODEL == "bart_mnli":
-        from cybersec_classifier.bart_mnli import BartMNLIClassifier
-
-        return BartMNLIClassifier()
-    else:
-        raise ValueError(f"Unknown model {Config.MODEL!r}")
